fcc2cc60224880fa7324d80cec6214191f6e1c75
MicahJank <MicahJank@users.noreply.github.com>
2019-07-22 22:11:55 -0500

    added more functions to create the right DOM elements as well as added them to the DOM

--- components/index.js
+++ components/index.js
@@ -11,3 +11,61 @@ const createTitle = () => {
 const mainTitle = createTitle();
 const titleSection = document.querySelector('.title');
 titleSection.appendChild(mainTitle);
+
+
+// create main element function
+const createMainDiv = () => {
+    const mainDiv = document.createElement('div');
+
+    return mainDiv
+
+};
+
+// create the slidersDiv
+const createSliders = () => {
+    const slidersDiv = document.createElement('div');
+
+    const borderRadius = document.createElement('input');
+    borderRadius.type = 'range';
+    borderRadius.min = 0;
+    borderRadius.max = 100;
+    borderRadius.value = 0;
+
+    slidersDiv.appendChild(borderRadius);
+
+    return slidersDiv;
+};
+
+
+// add main element and slidersDiv to the DOM and then attach them to the main section
+const mainSection = document.querySelector('.main');
+const mainDiv = createMainDiv();
+const slidersDiv = createSliders();
+mainSection.appendChild(mainDiv);
+mainSection.appendChild(slidersDiv);
+
+
+// create the copy to clipboard part
+const createCodePreview = () => {
+    const codeInput = document.createElement('input');
+    codeInput.type = 'text';
+    codeInput.readOnly = true;
+
+    return codeInput;
+};
+
+const createCopyClipboard = () => {
+    const copyClipboard = document.createElement('button');
+    copyClipboard.textContent = 'Copy Code';
+
+    return copyClipboard;
+};
+
+
+// add the code preview and copy clipboard to the DOM
+const codeSection = document.querySelector('.code');
+const codePreview = createCodePreview();
+const copyClipboard = createCopyClipboard();
+
+codeSection.appendChild(codePreview);
+codeSection.appendChild(copyClipboard);
