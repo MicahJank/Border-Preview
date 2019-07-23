890acfc8d58dfb718e2f9ea519c2123799337118
MicahJank <MicahJank@users.noreply.github.com>
2019-07-22 19:47:02 -0500

    created the created title function and added it to the DOM

---
+++ components/index.js
@@ -0,0 +1,12 @@
+// Create title function
+const createTitle = () => {
+    const title = document.createElement('h1');
+    title.classList.add('main-title');
+
+    return title;
+};
+
+// add the title to the DOM
+const mainTitle = createTitle();
+const titleSection = document.querySelector('.title');
+titleSection.appendChild(mainTitle);
