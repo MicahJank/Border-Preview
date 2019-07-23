b7431a7da7f1612576ed38efadcbe4523803ae34
MicahJank <MicahJank@users.noreply.github.com>
2019-07-22 20:27:49 -0500

    added the reset code to the reset.less file

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 // Create title function
 const createTitle = () => {
     const title = document.createElement('h1');
+    title.textContent = 'Border Radius Preview';
     title.classList.add('main-title');
 
     return title;
